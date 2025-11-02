309806c42fabefea0b05c9d18f38aa53257b85f1
Dima <399221@gmail.com>
2025-11-03 02:34:14 +0400

    modified:   LPP_SDK/configs/lpp_config_f103.h 	modified:   LPP_SDK/configs/lpp_config_f411.h 	modified:   LPP_SDK/configs/lpp_config_g431.h 	modified:   LPP_SDK/lpp_sdk.c 	modified:   LPP_SDK/lpp_sdk.h

---
+++ LPP_SDK/configs/lpp_config_g431.h
@@ -0,0 +1,28 @@
+/**
+ ******************************************************************************
+ * @file    lpp_config_g431.h
+ * @brief   LPP hardware configuration for STM32G431
+ *
+ * This file is part of the LPP (Laser Printer Platform) SDK —
+ * a software development kit for embedded laser printer control,
+ * designed for STM32 microcontrollers.
+ *
+ * Copyright (C) 2025  Dima
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Official channel: https://t.me/LPP_Printer
+ * Community chat:    https://t.me/LPP_Printer_Chat
+ ******************************************************************************
+ */
--- LPP_SDK/lpp_sdk.h
+++ LPP_SDK/lpp_sdk.h
@@ -1,9 +1,32 @@
 /**
  ******************************************************************************
  * @file    lpp_sdk.h
- * @brief   Printer SDK — global variables and structures declarations
+ * @brief   Laser Printer Platform (LPP) — SDK global declarations
+ *
+ * This file is part of the LPP (Laser Printer Platform) SDK — 
+ * a software development kit for embedded laser printer control,
+ * designed for STM32 microcontrollers (Blue Pill, Black Pill, G4 series).
+ *
+ * Copyright (C) 2025  Dima
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Official channel: https://t.me/LPP_Printer
+ * Community chat:    https://t.me/LPP_Printer_Chat
  ******************************************************************************
  */
+
 
 #ifndef LPP_SDK_H
 #define LPP_SDK_H
