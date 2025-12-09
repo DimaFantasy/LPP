f46d9db020d51817fc2748e6a1ad8e165dc52977
Dima <399221@gmail.com>
2025-12-09 15:40:21 +0400

    new file:   LPP_SDK/configs/lpp_config_g431_alt.h 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/LPP_STM32F103Cx.axf 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/LPP_STM32F103Cx.build_log.htm 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/LPP_STM32F103Cx.hex 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/LPP_STM32F103Cx.htm 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/LPP_STM32F103Cx_LPP_STM32F103Cx.dep 	modified:   LPP_STM32F103Cx/MDK-ARM/LPP_STM32F103Cx/main.o 	modified:   LPP_STM32F103Cx/MDK-ARM/copy.bat 	modified:   LPP_STM32F103Cx/MDK-ARM/counter.txt 	modified:   LPP_STM32F103Cx/My_Libraries/counter.h 	renamed:    LPP_STM32G431Cx/PIN.txt -> LPP_STM32G431Cx_ALT/STM32G431Cx_AltPinout.txt 	modified:   PCB/README.md

--- LPP_STM32F103Cx/MDK-ARM/copy.bat
+++ LPP_STM32F103Cx/MDK-ARM/copy.bat
@@ -5,7 +5,7 @@ set "batdir=%~dp0"
 :: Исходный файл: LPP.hex рядом в подпапке LPP
 set "source=%batdir%LPP_STM32F103Cx\LPP_STM32F103Cx.hex"
 :: Целевая папка: подняться на 3 уровня до \, потом в DELPHI_PROJEKT\...
-set "destination=%batdir%..\..\..\DELPHI_PROJEKT\XE7\bin\update\STM32F103Cx\STM32F103Cx_app.hex"
+set "destination=%batdir%..\..\..\DELPHI_PROJEKT\XE7\bin\update\STM32F103Cx\!STM32F103Cx_app.hex"
 :: Копируем с новым именем
 copy "%source%" "%destination%" /Y
 echo File copied and renamed successfully.
--- LPP_STM32F103Cx/My_Libraries/counter.h
+++ LPP_STM32F103Cx/My_Libraries/counter.h
@@ -1,4 +1,4 @@
 #ifndef COUNTER_H
 #define COUNTER_H
-#define APP_VERSION 2423U
+#define APP_VERSION 2424U
 #endif
